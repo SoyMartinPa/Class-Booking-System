1e3b412ec2e932cd4b7203530bd1f8fdb5a273d7
SoyMartinPa <marparra2828@gmail.com>
2026-08-10 18:16:24 -0400

    docs: update documentation

--- src/main/java/Logica/Reservas/EstadoReserva.java
+++ src/main/java/Logica/Reservas/EstadoReserva.java
@@ -7,8 +7,22 @@ import Logica.Perfiles.Tutor.Tutor;
  * Define el comportamiento asociado a los distintos estados de una reserva.
  *
  * <p>Cada implementación determina qué operaciones son válidas según el
- * estado actual de la reserva y cómo afectan su ciclo de vida. En un futuro se pueden extender
- * los estados 'pagoPendiente','completadaParcialmente', etc.</p>
+ * estado actual de la reserva y cómo afectan su ciclo de vida. En un futuro se pueden
+ * extender los estados ('pagoPendiente', 'completadaParcialmente', etc.) sin modificar
+ * la clase {@code GestorReserva}, ya que esta delega el comportamiento en su estado actual
+ * en lugar de conocer las reglas específicas de cada uno, disminuyendo así el
+ * acoplamiento entre ambas.</p>
+ *
+ * <p>Los estados de la reserva se pueden separar entre "Finalizados" y "Pendientes".
+ * Entre los primeros están los estados 'Cancelada' y 'Completada', y en el grupo de
+ * "Pendientes" se puede encontrar el estado 'Pendiente'. Las reservas "Finalizadas"
+ * lanzarán excepciones al intentar realizar modificaciones sobre la reserva (como
+ * agregar estudiantes o cambios de estado), mientras que las "Pendientes" dependerán
+ * de su estado específico, pero no lanzarán excepciones.</p>
+ *
+ * <p>Este comportamiento se documenta explícitamente como parte del contrato de
+ * {@code EstadoReserva}, de modo que ninguna implementación viola las expectativas
+ * del cliente que la invoca (Principio de Sustitución de Liskov).</p>
  */
 public interface EstadoReserva {
     /**
